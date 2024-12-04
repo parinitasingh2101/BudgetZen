fae10c9c84667798d2ad388f5b9d8b42786094ee
Parinita Singh <parinitasingh2101@gmail.com>
2024-12-04 22:13:05 +0530

    currency

--- lib/utils.ts
+++ lib/utils.ts
@@ -15,9 +15,9 @@ export function convertAmountToMiliunits(amount: number) {
 }
 
 export function formatCurrency(value: number) {
-  return new Intl.NumberFormat("en-US", {
+  return new Intl.NumberFormat("en-IN", {
     style: "currency",
-    currency: "USD",
+    currency: "INR",
     minimumFractionDigits: 2,
   }).format(value);
 }
